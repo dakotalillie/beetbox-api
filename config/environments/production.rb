b93fbe1322ca4f4d0a5a449dc41c4272a4d97461
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-25 15:11:43 -0500

    config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-
+  config.web_socket_server_url = "wss://beetbox-api.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://beetbox.herokuapp.com', 'http://beetbox.herokuapp.com']
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
