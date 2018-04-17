420becaeb328b892d6c6a11721a769ce8f48e587
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-17 00:20:01 -0400

    remove paperclip default config

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -9,8 +9,8 @@ class Sample < ApplicationRecord
                     processors: [:ffmpeg_wav_to_mp3],
                     :path => ':user_id/:style/:filename',
                     :storage => :s3,
-                    # :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    # :s3_region => 'us-east-1'
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :s3_region => 'us-east-1'
   validates_attachment_file_name :fullres_file, :matches => [/wav\Z/, /Wav\Z/,/mpe?g\Z/]
   do_not_validate_attachment_file_type :fullres_file
 
@@ -18,7 +18,7 @@ class Sample < ApplicationRecord
     attachment.instance.user.present? ? attachment.instance.user.id : 'dev'
   end
   
-  # def s3_credentials
-  #   {:bucket => "beetbox-data", :access_key_id => ENV['aws_access_key'], :secret_access_key => ENV['aws_secret_access_key']}
-  # end
+  def s3_credentials
+    {:bucket => "beetbox-data", :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,14 +46,14 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: ENV['AWS_REGION'],
-    }
-  }
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV['S3_BUCKET_NAME'],
+  #     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  #     s3_region: ENV['AWS_REGION'],
+  #   }
+  # }
   
 end
